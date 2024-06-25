b3c92796a62cce5d761de226b92d09cd09ba8545
osalino <osazemen.odeh@gmail.com>
2024-06-25 19:03:58 +0100

    HTML CSS Completed //

---
+++ index.js
@@ -0,0 +1,13 @@
+const job = [
+    {
+
+        id: 1,
+        title: "Software Engineer",
+        company: "Google",
+        location: "Mountain View, CA",
+        description: "Design, develop, and test software applications.",
+    }
+]
+
+
+export default job;  // export the job array as default export
